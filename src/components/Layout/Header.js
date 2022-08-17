4ec00c9a51836d81cad04972b810b7148af69d4a
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-08-17 12:16:47 -0400

    Initial commit
    Initial commit includes some component with basic jsx and styling
    
    Signed-off-by: Anastasiia Boryslavska <anastasiiaforwork1@gmail.com>

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import {Fragment} from "react";
+import mealsImage from '../../assests/meals.jpeg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header() {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton/>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food'/>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Header;
