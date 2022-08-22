36cb4212e2cba32e9e467bfb94c477e064919a90
Anastasiia Boryslavska <anastasiiaforwork@gmail.com>
2022-08-21 21:20:16 -0400

    Header Cart Button
    Header Cart Button component was created
    
    Signed-off-by: Anastasiia Boryslavska <anastasiiaforwork1@gmail.com>

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,17 @@
+import { useContext } from 'react'
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
-import React from "react";
+import CartContext from '../../store/cart-context'
 
 function HeaderCartButton(props) {
+
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
@@ -11,7 +19,7 @@ function HeaderCartButton(props) {
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>
-        3
+        {numberOfCartItems}
       </span>
     </button>
   );
